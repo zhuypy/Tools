c0ecc6014ae5310e70a09fec7227da246bf50af8
zhuypy <1007531447@qq.com>
2021-01-04 09:16:06 +0800

    20200104

--- reptile/reptile_cartoon.py
+++ reptile/reptile_cartoon.py
@@ -1,13 +1,47 @@
 from util import *
+import socket
+socket.setdefaulttimeout(20)
 
+LOG_FILE = r'D:\test\cartoon.log'
+SAVE_PATH = r'D:\test\cartoon'
 
+def logWrite_cartoon(msg):
+    logWrite(msg,LOG_FILE)
 
+def getChapterList(url_home):
+    resp = vistUrl(url_home, False)
+    pattern = r'<a href="(.{0,10}[0-9]{3}/)"'
+    chapterList = getUrlList(pattern,resp.text)
+    resp.close()
+    return [url_home+i for i in chapterList]
 
+def getIndexHtmlList(url_chapter):
+    resp = vistUrl(url_chapter, False)
+    pattern = r'<a href="(index_[0-9]{1,3}.html)"'
+    chapterList = getUrlList(pattern, resp.text)
+    resp.close()
+    return [url_chapter + i for i in chapterList]
 
+def getImgList(url_html):
+    resp = vistUrl(url_html, False)
+    pattern = r'<img src="(https:.*?.jpg)"'
+    url_img = getUrlList(pattern, resp.text)
+    print(resp.text)
 
+def test():
+    url_home = 'https://www.fzdm.com/manhua/74/'
+    chapterList = getChapterList(url_home)
+    # for url_charpter in chapterList:
+    #     getIndex(url_charpter)
+    index_html_list = getIndexHtmlList(chapterList[0])
+    getImgList(index_html_list[0])
+    
 
-
-
+if __name__ == '__main__':
+    initFilePath(LOG_FILE,isFile=True,clear=True)
+    initFilePath(SAVE_PATH)
+    test()
+    
 
 
 
--- reptile/util.py
+++ reptile/util.py
@@ -3,6 +3,7 @@ import os
 import shutil
 import urllib.request
 import urllib
+import re
 
 
 def vistUrl(url,isGetJson=True):
@@ -13,7 +14,8 @@ def vistUrl(url,isGetJson=True):
     :return:默认返回字典，若isGetJson=False，则返回response对象
     '''
     headers = {
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36'
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36',
+        'cookie':'_ga=GA1.1.314232902.1609310189; picHost=http%3A//www-mipengine-org.mipcdn.com/i/p3.manhuapan.com; Hm_lvt_eaa57ca47dacb4ad4f5a257001a3457c=1609310206,1609310240; _ga_1FZE0C2L80=GS1.1.1609314430.2.1.1609314677.0; Hm_lpvt_eaa57ca47dacb4ad4f5a257001a3457c=1609314684'
     }
     respose = requests.get(url, headers=headers)
     if isGetJson:
@@ -21,13 +23,13 @@ def vistUrl(url,isGetJson=True):
     else:
         return respose
 
-def initFilePath(path,clear=False):
+def initFilePath(path,isFile=False,clear=False):
     '''
     初始化文件目录
     :param save_path:
     :return:
     '''
-    if os.path.isfile(path):
+    if isFile:
         if os.path.exists(path) and clear:
             os.remove(path)
     else:
@@ -52,3 +54,9 @@ def saveImg(img_url,img_name,img_file):
             print('下载【%s】至文件夹【%s】，完成' % (img_name, img_file))
     except urllib.error.HTTPError as e:
         logWrite(e.reason)
+
+
+def getUrlList(pattern,html_str):
+    # pattern = r'<a href="(https.*?/[0-9]{9})".*'
+    url_list = re.findall(pattern, html_str)
+    return list(set(url_list))
